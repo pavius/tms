193464ab4bf157c354bbca0767ec6f78b901eda3
Eran Duchan <pavius@gmail.com>
2014-06-06 11:10:26 +0300

    - nothing

--- server/src/models/patient.js
+++ server/src/models/patient.js
@@ -1,11 +1,15 @@
 var mongoose = require('mongoose');
+var Payment = require('appointment');
+var Appointment = require('payment');
 
 var patientSchema = mongoose.Schema
 ({
     name: {type: String, required: true},
     email: {type: String, required: true},
     primary_phone: {type: String},
-    appointment_price: {type: Number, min: 0, default: 330}
+    appointment_price: {type: Number, min: 0, default: 330},
+    appointments: [Appointment.schema],
+    payments: [Payment.schema]
 });
 
 module.exports = mongoose.model('Patient', patientSchema);
